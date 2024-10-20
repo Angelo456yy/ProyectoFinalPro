28fd183d054523d7e91de6ef36bb621655c74a57
Angel Martinez <134995261+Angelo456yy@users.noreply.github.com>
2024-10-20 17:17:39 -0600

    alpha 0.4 implementacion de migracion

--- Traveling/app/build.gradle.kts
+++ Traveling/app/build.gradle.kts
@@ -30,6 +30,15 @@ android {
             )
         }
     }
+
+    buildFeatures {
+        compose = true
+    }
+
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.3" // Asegúrate de usar la versión adecuada
+    }
+    
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
