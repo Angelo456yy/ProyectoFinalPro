b82467e1fbe1adc5d1c56fb27db04bdf971dd2a4
majitomoomin <mariajoseportill2@gmail.com>
2024-10-22 00:35:22 -0600

    views, navigation y Main cod

--- app/src/main/java/com/example/proyectoversion22/MainActivity.kt
+++ app/src/main/java/com/example/proyectoversion22/MainActivity.kt
@@ -1,22 +1,48 @@
 package com.example.proyectoversion22
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.proyectoversion22.navigation.NavManager
+import com.example.proyectoversion22.ui.theme.DemoNavigationTheme
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        setContent {
+            DemoNavigationTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    NavManager()
+                }
+            }
         }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+
+@Composable
+fun GreetingPreview() {
+    DemoNavigationTheme {
+        Greeting("Android")
     }
 }
 
--- app/src/main/java/com/example/proyectoversion22/views/DetailView.kt
+++ app/src/main/java/com/example/proyectoversion22/views/DetailView.kt
@@ -1,2 +1,69 @@
 package com.example.proyectoversion22.views
 
+import android.annotation.SuppressLint
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import com.example.proyectoversion22.components.MainButton
+import com.example.proyectoversion22.components.MainIconButton
+import com.example.proyectoversion22.components.Space
+import com.example.proyectoversion22.components.TitleBar
+import com.example.proyectoversion22.components.TitleView
+
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DetailView(navController: NavController,id: Int, opcional: String?) {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { TitleBar(name = "Detail View") },
+                colors = TopAppBarDefaults.mediumTopAppBarColors(
+                    containerColor = Color.Blue
+                ),
+                navigationIcon = {
+                    MainIconButton(icon = Icons.Default.ArrowBack) {
+                        navController.popBackStack()
+                    }
+                }
+            )
+        }
+    ) {
+        ContentDetailView(navController, id, opcional)
+    }
+}
+
+@Composable
+fun ContentDetailView(navController: NavController,id: Int, opcional: String?) {
+    Column (
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+        TitleView(name = "Detail View")
+        Space()
+        TitleView(name = id.toString())
+        if (opcional==""){
+            Spacer(modifier = Modifier.height(0.dp))
+        }else{
+            TitleView(name = opcional.orEmpty())
+        }
+        MainButton(name = "Return Home", backColor = Color.Blue, color = Color.White) {
+            navController.popBackStack();
+        }
+    }
+}
