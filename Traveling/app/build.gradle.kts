fc4f8f6748317835a81df5e8db867a5ad6554cb7
Angel Martinez <134995261+Angelo456yy@users.noreply.github.com>
2024-10-25 00:50:41 -0600

    alpha 0.7.4.2 actualizacion de diseño

--- Traveling/app/build.gradle.kts
+++ Traveling/app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     id("com.google.gms.google-services")
+    id ("kotlin-kapt")
 }
 
 android {
@@ -50,19 +51,19 @@ dependencies {
     implementation(platform("com.google.firebase:firebase-bom:33.4.0"))
     implementation("com.google.firebase:firebase-auth-ktx")
     implementation("com.google.firebase:firebase-database-ktx")
-    implementation("com.google.firebase:firebase-firestore-ktx")
-    implementation("com.google.firebase:firebase-storage-ktx")
 
     // Material Components
-    implementation("androidx.compose.material3:material3:1.1.0")
-    implementation("com.google.android.material:material:1.6.0")
+    implementation("androidx.compose.material3:material3:1.1.0") // Asegúrate de usar una versión compatible
+    implementation("com.google.android.material:material:1.6.0") // Versión más reciente
 
     // Compose
     implementation("androidx.compose.ui:ui:1.4.3")
     implementation("androidx.compose.ui:ui-tooling-preview:1.4.3")
-    implementation("androidx.activity:activity-compose:1.9.2")
-    implementation(libs.androidx.activity)
     debugImplementation("androidx.compose.ui:ui-tooling:1.4.3")
+    implementation("androidx.activity:activity-compose:1.9.2")
+    debugImplementation("androidx.compose.ui:ui-tooling:1.4.3") // Asegúrate de usar la versión correcta
+
+
 
     // Core and UI
     implementation("androidx.core:core-ktx:1.10.0")
@@ -77,12 +78,10 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    // UI Tests
     androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.4.3")
     debugImplementation("androidx.compose.ui:ui-test-manifest:1.4.3")
-    testImplementation("org.mockito:mockito-core:4.6.1")
-    androidTestImplementation("androidx.test.espresso:espresso-contrib:3.5.1")
-
-    // GIF
-    implementation("com.google.accompanist:accompanist-coil:0.15.0") // Versión estable anterior
+    implementation("com.github.bumptech.glide:glide:4.15.0")
+    kapt("com.github.bumptech.glide:compiler:4.14.2")
 }
-
